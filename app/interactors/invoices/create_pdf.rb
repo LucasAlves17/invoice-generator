f945a3c3a3081252ea805630af0a17989b91df01
LucasAlves17 <lukasmartinsalves17@gmail.com>
2023-01-20 15:59:38 -0300

    extract pdf creation to an interactor

---
+++ app/interactors/invoices/create_pdf.rb
@@ -0,0 +1,13 @@
+class Invoices::CreatePdf
+  include Interactor
+
+  def call
+    pdf = Prawn::Document.new
+    pdf.text "Invoice number: #{context.params[:number]}"
+    pdf.text "Date: #{context.params[:date]}"
+    pdf.text "Company: #{context.params[:company]}"
+    pdf.text "Charge for: #{context.params[:charge_for]}"
+    pdf.text "Total: #{context.params[:total_in_cents].to_f / 100}"
+    pdf
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   namespace :api, defaults: { format: :json} do
-    resources :invoices
+    resources :invoices, only: [:create, :show, :index, :update]
   end 
 end
