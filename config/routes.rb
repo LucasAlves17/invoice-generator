7c2262e2e172faaa1849c3bad2b5cedb6a8b7380
LucasAlves17 <lukasmartinsalves17@gmail.com>
2023-01-23 13:31:07 -0300

    add confirm_user to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   namespace :api, defaults: { format: :json} do
     resources :invoices, only: [:create, :show, :index, :update]
+    
+    resources :users, only: [:create]
+    get '/users/confirm', to: 'users#confirm', as: 'confirm_users'
   end 
 end
