0ca8311fee5b65cccf87133c0780a7edfdcc2504
LucasAlves17 <lukasmartinsalves17@gmail.com>
2023-01-24 18:19:45 -0300

    add invoice filters

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,3 +1,8 @@
 class Invoice < ApplicationRecord
+  include Filterable
+
   validates_presence_of :number, :date, :company, :charge_for, :total_in_cents, :emails
+
+  scope :filter_by_number, -> (number) { where number: number }
+  scope :filter_by_date, -> (date) { where date: date }
 end
