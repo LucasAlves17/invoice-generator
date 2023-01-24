20fb674dc589c0f2aa44480e72c81a70ff83ae11
LucasAlves17 <lukasmartinsalves17@gmail.com>
2023-01-24 18:20:30 -0300

    fix user controller tests

--- spec/controllers/api/users_controller_spec.rb
+++ spec/controllers/api/users_controller_spec.rb
@@ -5,29 +5,61 @@ RSpec.describe Api::UsersController, type: :controller do
     context "when successful" do
       let(:email) { Faker::Internet.email }
       before do
-        expect(User::GenerateToken).to receive(:call).once.with(email: email).and_return(context)
+        expect(Users::GenerateToken).to receive(:call).once.with(email: email).and_return(context)
       end
 
-      let(:context) { double(:context, success?: true, message: "A confirmation email has been sent to your email address. Please check your inbox to activate your account.") }
+      let(:context) { double(:context, success?: true ) }
 
-      it "show a message that an email was send" do
+      it "return 201" do
         response = post :create, params: { email: email }
 
-        expect(response.parsed_body["message"]).to eq(context.message)
+        expect(response.status).to eq(201)
       end
     end
 
     context "when unsuccessful" do
       before do
-        expect(User::GenerateToken).to receive(:call).once.with(email: "").and_return(context)
+        expect(Users::GenerateToken).to receive(:call).once.with(email: "").and_return(context)
       end
 
       let(:context) { double(:context, success?: false, errors: "Email is missing") }
 
-      it "show a error message" do
+      it "return 422" do
         response = post :create, params: { email: "" }
 
-        expect(response.parsed_body["errors"]).to eq(context.errors)
+        expect(response.status).to eq(422)
+      end
+    end
+  end
+
+  describe "#confirm" do
+    context "when successful" do
+      let(:user_to_confirm) { create :user_to_confirm }
+
+      before do
+        expect(Users::ConfirmToken).to receive(:call).once.with(token_to_confirm: user_to_confirm[:token_to_confirm]).and_return(context)
+      end
+
+      let(:context) { double(:context, success?: true ) }
+
+      it "return 200" do
+        response = get :confirm, params: { token: user_to_confirm[:token_to_confirm] }
+
+        expect(response.status).to eq(200)
+      end
+    end
+
+    context "when unsuccessful" do
+      before do
+        expect(Users::ConfirmToken).to receive(:call).once.with(token_to_confirm: "123").and_return(context)
+      end
+
+      let(:context) { double(:context, success?: false, errors: "Invalid token") }
+
+      it "return 422" do
+        response = get :confirm, params: { token: "123" }
+
+        expect(response.status).to eq(422)
       end
     end
   end
